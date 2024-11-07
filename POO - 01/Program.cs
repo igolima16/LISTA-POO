3d5b20ebfdacb43b680b3430c61cf55d2f5a5185
igolima16 <167836742+igolima16@users.noreply.github.com>
2024-11-07 14:42:54 -0300

    Add files via upload

---
+++ POO - 01/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POO___01
+{
+    public class Retangulo
+    { 
+        public double Base { get; set; }
+        public double Altura { get; set; }
+
+        public Retangulo(double baseRetangulo, double alturaRetangulo)
+        {
+            Base = baseRetangulo;
+            Altura = alturaRetangulo;
+        }
+
+        public double CalcularArea()
+        {
+            return Base * Altura;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite a base do retângulo: ");
+            double baseRetangulo = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Digite a altura do retângulo: ");
+            double alturaRetangulo = Convert.ToDouble(Console.ReadLine());
+
+            
+            Retangulo retangulo = new Retangulo(baseRetangulo, alturaRetangulo);
+
+     
+            double area = retangulo.CalcularArea();
+            Console.WriteLine($"A área do retângulo é: {area}");
+
+            Console.ReadKey();
+        }
+    }
+}
