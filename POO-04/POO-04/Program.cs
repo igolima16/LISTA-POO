6d2bbbf3ff53e081354fc79057ecd323eee67ee3
igolima16 <167836742+igolima16@users.noreply.github.com>
2024-11-07 16:20:00 -0300

    Add files via upload

---
+++ POO-04/POO-04/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace CalculoAreaTriangulo
+{
+    
+    public class Triangulo
+    {
+        public double Base { get; set; }
+        public double Altura { get; set; }
+
+        public Triangulo(double baseTriangulo, double alturaTriangulo)
+        {
+            Base = baseTriangulo;
+            Altura = alturaTriangulo;
+        }
+
+        public double CalcularArea()
+        {
+            return (Base * Altura) / 2;
+        }
+    }
+
+    // Classe principal
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Entrada do usuário
+            Console.Write("Digite o valor da base do triângulo: ");
+            double baseTriangulo = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Digite o valor da altura do triângulo: ");
+            double alturaTriangulo = Convert.ToDouble(Console.ReadLine());
+
+            // Instância do objeto Triangulo
+            Triangulo triangulo = new Triangulo(baseTriangulo, alturaTriangulo);
+
+            // Cálculo e exibição da área
+            double area = triangulo.CalcularArea();
+            Console.WriteLine($"A área do triângulo é: {area}");
+
+            Console.ReadKey();
+        }
+    }
+}
