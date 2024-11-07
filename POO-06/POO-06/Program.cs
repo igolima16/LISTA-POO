3e049617346f5243fb7d1f5b0f8dc15715dfeea3
igolima16 <167836742+igolima16@users.noreply.github.com>
2024-11-07 16:21:06 -0300

    Add files via upload

---
+++ POO-06/POO-06/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace ConversaoMilhasMaritimas
+{
+  
+    public class Conversor
+    {
+      
+        public double ConverterMilhasParaQuilometros(double milhasMaritimas)
+        {
+            double metros = milhasMaritimas * 1852;    
+            double quilometros = metros / 1000;        
+            return quilometros;
+        }
+    }
+
+   
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Entrada do usuário
+            Console.Write("Digite o valor em milhas marítimas: ");
+            double milhasMaritimas = Convert.ToDouble(Console.ReadLine());
+
+            // Instância do objeto Conversor
+            Conversor conversor = new Conversor();
+
+            // Conversão e exibição do resultado
+            double quilometros = conversor.ConverterMilhasParaQuilometros(milhasMaritimas);
+            Console.WriteLine($"{milhasMaritimas} milhas marítimas equivalem a {quilometros} quilômetros.");
+
+            Console.ReadKey();
+        }
+    }
+}
+   
