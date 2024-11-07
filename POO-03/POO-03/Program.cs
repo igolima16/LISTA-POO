f3a72936962cceff9b75a876c86ddcec1ffe933b
igolima16 <167836742+igolima16@users.noreply.github.com>
2024-11-07 16:20:28 -0300

    Add files via upload

---
+++ POO-03/POO-03/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace CalculoAreaQuadrado
+{
+    public class Quadrado
+    {
+        public double Diagonal { get; set; }
+
+        public Quadrado(double diagonal)
+        {
+            Diagonal = diagonal;
+        }
+
+        public double CalcularArea()
+        {
+            return (Diagonal * Diagonal) / 2;
+        }
+    }
+
+    // Classe principal
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Entrada do usuário
+            Console.Write("Digite o valor da diagonal do quadrado: ");
+            double diagonal = Convert.ToDouble(Console.ReadLine());
+
+            // Instância do objeto Quadrado
+            Quadrado quadrado = new Quadrado(diagonal);
+
+            // Cálculo e exibição da área
+            double area = quadrado.CalcularArea();
+            Console.WriteLine($"A área do quadrado é: {area}");
+
+            Console.ReadKey();
+        }
+    }
+}
